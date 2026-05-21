917f7c9cf6e491e60f205aea5f59d1e5d6a7251b
edward kubiak <97137083+ek33450505@users.noreply.github.com>
2026-05-21 12:15:03 -0400

    chore: bump cast-desktop to 1.2.5

--- Casks/cast-desktop.rb
+++ Casks/cast-desktop.rb
@@ -1,6 +1,6 @@
 cask "cast-desktop" do
-  version "1.2.4"
-  sha256 "3af1abb3d206e90e2a21b0bc47cfd5705665a7700043d4c5ed60e5fe0e712b30"
+  version "1.2.5"
+  sha256 "0060ee8c545f75abe3df3d576df1ea9d8630c68c2f510faf036f9ca92237eeae"
 
   url "https://github.com/ek33450505/cast-desktop/releases/download/v#{version}/Cast-Desktop-aarch64-darwin.tar.gz"
   name "Cast Desktop"
